4474af723b17335e0480b3b27d85e74a00477dea
Filomen Incahuanaco Quispe <fincahuanaco@usp.br>
2021-06-22 16:51:26 -0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,15 @@
+
+howtime ./p2obj -i ../../../build/bin/pFrame000210.obj -o pFrame210_ani_k0.015_r256_g0.01.obj -k 0.015 -r 256,256,256 -g 0.01,0.01,0.01
+
+
+ ./p2obj -i ../../../build/bin/pFrame000210.obj -o pFrame210_spherical.obj -m spherical -k 0.02 -r 256,256,256
+
+
+./p2obj -i ../../../build/bin/pFrame000210.obj -o pFrame210_sph.obj -m sph -k 0.05 -r 256,256,256
+
+
+./p2obj -i ../../../build/bin/pFrame000210.obj -o pFrame210_zhu.obj -k 0.05 -m zhu_bridson
+
+./p2obj -i ../../../build/bin/pFrame000210.obj -o pFrame210_ani.obj -k 0.08 -r 512,512,512
+
+
